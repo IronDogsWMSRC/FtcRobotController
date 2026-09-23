56e34f310b8be8bfc86cfd9d347317bc91575f08
WMS Robotics Club <pccswestrobotics@gmail.com>
2026-09-23 12:31:33 -0400

    Create HelloWorld.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Autonomous
+public class HelloWorld extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "IronDogs");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    // Single Line Comment
+    /*
+    1. Hello World, change the telemetry data to display "Hello, Your Name"
+    2. Run the code in the Automous section of your DS
+     */
+}
